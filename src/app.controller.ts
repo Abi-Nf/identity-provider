96efef00fb8273652c7f50468fa3cd2d24ae381c
Abi-Nf <hei.abigail.3@gmail.com>
2025-01-13 19:42:41 +0300

    feat: add GET /ping & /list

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FirebaseGuard } from './firebase/firebase.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('ping')
+  ping(): string {
+    return this.appService.getPong();
+  }
+
+  @Get('list')
+  @UseGuards(FirebaseGuard)
+  getList() {
+    return this.appService.getList();
   }
 }
