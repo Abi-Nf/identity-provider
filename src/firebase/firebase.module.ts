45b92163d42e51aa9905478d925bbacf84e9b565
Abi-Nf <hei.abigail.3@gmail.com>
2025-01-13 19:38:53 +0300

    feat: firebase module

---
+++ src/firebase/firebase.module.ts
@@ -0,0 +1,41 @@
+import { FactoryProvider, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as admin from 'firebase-admin';
+import { FirebaseGuard } from './firebase.guard';
+import { FirebaseRepository } from './firebase.repository';
+import { FirebaseService } from './firebase.service';
+
+const provider: FactoryProvider<admin.app.App> = {
+  provide: 'FIREBASE_APP',
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => {
+    const firebaseConfig = {
+      type: configService.get<string>('TYPE'),
+      project_id: configService.get<string>('PROJECT_ID'),
+      private_key_id: configService.get<string>('PRIVATE_KEY_ID'),
+      private_key: configService
+        .get<string>('PRIVATE_KEY')
+        .replaceAll('\\n', '\n'),
+      client_email: configService.get<string>('CLIENT_EMAIL'),
+      client_id: configService.get<string>('CLIENT_ID'),
+      auth_uri: configService.get<string>('AUTH_URI'),
+      token_uri: configService.get<string>('TOKEN_URI'),
+      auth_provider_x509_cert_url: configService.get<string>('AUTH_CERT_URL'),
+      client_x509_cert_url: configService.get<string>('CLIENT_CERT_URL'),
+      universe_domain: configService.get<string>('UNIVERSAL_DOMAIN'),
+    } as admin.ServiceAccount;
+
+    return admin.initializeApp({
+      credential: admin.credential.cert(firebaseConfig),
+      databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`,
+      storageBucket: `${firebaseConfig.projectId}.appspot.com`,
+    });
+  },
+};
+
+@Module({
+  imports: [ConfigModule],
+  providers: [provider, FirebaseRepository, FirebaseService, FirebaseGuard],
+  exports: [FirebaseRepository, FirebaseService, FirebaseGuard],
+})
+export class FirebaseModule {}
