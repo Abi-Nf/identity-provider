8603ba4d1c7bd93676f9434ba796000e00e24760
Abi-Nf <hei.abigail.3@gmail.com>
2025-01-13 19:40:37 +0300

    chore: add getPong and getList example

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getPong(): string {
+    return 'pong';
+  }
+
+  getList(){
+    return {
+      list: [{ message: 'you got a list content' }],
+    };
   }
 }
